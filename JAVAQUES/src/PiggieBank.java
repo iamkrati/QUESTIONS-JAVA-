53c5ef01c03e8112e2791e33ae65c0106f8483d2
KRATI GOYAL <gkrati04@gmail.com>
2021-11-17 12:53:53 +0530

    QUESTION 6 PiggieBank

--- JAVAQUES/src/PiggieBank.java
+++ JAVAQUES/src/PiggieBank.java
@@ -2,18 +2,18 @@ public class PiggieBank {
     public static void main(String[] args) {
         AddAmount obj1=new AddAmount();
         AddAmount obj=new AddAmount(50);
-
+        System.out.println(AddAmount.amount);
     }
 }
 class AddAmount{
-    int amount=50;
+    static int amount=50;
     AddAmount(){
-        System.out.println("No money added to the PiggieBank so amount is : "+amount);
+       // System.out.println("No money added to the PiggieBank so amount is : "+amount);
     }
     AddAmount(int addamount)
     {
         amount=amount+addamount;
-        System.out.println("Final amount in piggie bank is : "+amount);
+       // System.out.println("Final amount in piggie bank is : "+amount);
     }
 }
 
