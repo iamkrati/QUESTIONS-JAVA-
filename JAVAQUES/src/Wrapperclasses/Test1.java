87c7f80aedacedd0ebcadedcb2a4181c21fa1ee4
KRATI GOYAL <gkrati04@gmail.com>
2021-11-21 22:06:16 +0530

    Quiz 2

---
+++ JAVAQUES/src/Wrapperclasses/Test1.java
@@ -0,0 +1,12 @@
+package Wrapperclasses;
+
+class Test1
+{
+    static Integer I;
+    public static void main(String args[])
+    {
+        int m = I ;
+        System.out.println(m);
+    }
+}
+//generate null pointer exception
