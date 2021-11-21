291e83a214d5e60533e5ff42ad890ef604f1eaf1
KRATI GOYAL <gkrati04@gmail.com>
2021-11-21 22:21:26 +0530

    Quiz 3

---
+++ JAVAQUES/src/Wrapperclasses/Test2.java
@@ -0,0 +1,11 @@
+package Wrapperclasses;
+
+class Test2{
+    public static void main(String args[]){
+        Integer X = 10;
+        Integer Y= X;
+        X++;
+        System.out.println(X);
+        System.out.println(Y);
+        System.out.println(X==Y);
+    }}
