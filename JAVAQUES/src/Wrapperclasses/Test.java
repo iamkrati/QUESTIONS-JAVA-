cf2afff72458d9f7291a38d53153bc84ea4dbdac
KRATI GOYAL <gkrati04@gmail.com>
2021-11-21 21:59:06 +0530

    Quiz 1

---
+++ JAVAQUES/src/Wrapperclasses/Test.java
@@ -0,0 +1,13 @@
+package Wrapperclasses;
+
+class Test
+{
+    public static void main(String ar[])
+    {
+        int x = 10;
+        Integer y = new Integer(10);
+        Integer z=50;
+        System.out.println(x==y);
+        System.out.println(x==z);
+    }
+}
